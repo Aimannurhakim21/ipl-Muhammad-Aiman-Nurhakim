48efed177e2f22c2d742b1b8cedce8a4ef1f069a
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2023-10-23 19:31:51 +0700

    Perubahan sesuai prinsip open close

--- openclose/openclose/StandardCinema.java
+++ openclose/openclose/StandardCinema.java
@@ -4,4 +4,9 @@ public class StandardCinema extends Cinema {
     public StandardCinema(double price) {
         this.price = price;
     }
+
+    @Override
+    Double calculateAdminFee() {
+        return price * 10 / 100;
+    }
 }
