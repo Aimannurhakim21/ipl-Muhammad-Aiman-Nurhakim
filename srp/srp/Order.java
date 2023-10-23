92d191a2c8480f0e59d6564e115f3f20cc878cd5
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2023-10-23 17:11:17 +0700

    re format code

--- srp/srp/Order.java
+++ srp/srp/Order.java
@@ -1,31 +1,39 @@
 package srp;
 
-class Order { 
-    void calculateTotalSum() { 
-        /*...*/ 
-    } 
-    void getItems() { 
-        /*...*/ 
-    } 
-    void getItemCount() { 
-        /*...*/ 
-    } 
-    void addItem(Item item) { 
-        /*...*/ 
-    } 
-    void deleteItem(Item item) { 
-        /*...*/ 
-    } 
-    void printOrder() { 
-        /*...*/ 
-    } 
-    void showOrder() { 
-        /*...*/ 
-    } 
+class Order {
+    void calculateTotalSum() {
+        /* ... */
+    }
+
+    void getItems() {
+        /* ... */
+    }
+
+    void getItemCount() {
+        /* ... */
+    }
+
+    void addItem(Item item) {
+        /* ... */
+    }
+
+    void deleteItem(Item item) {
+        /* ... */
+    }
+
+    void printOrder() {
+        /* ... */
+    }
+
+    void showOrder() {
+        /* ... */
+    }
+
     void getDailyHistory() {
 
-    } 
+    }
+
     void getMonthlyHistory() {
 
-    } 
-} 
+    }
+}
