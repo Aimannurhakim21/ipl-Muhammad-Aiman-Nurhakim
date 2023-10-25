ab02007522bbd9b6cdc8a1ae7fa128fbeede30ee
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2023-10-25 11:01:28 +0700

    perubahan sesuai prinsip isp

--- isp/isp/Motorcycle.java
+++ isp/isp/Motorcycle.java
@@ -1,26 +1,18 @@
 package isp;
 
-class Motorcycle implements VehicleInterface {
-
-    // Can be implemented
-    @Override
-    public void drive() {
-
-    }
-
-    @Override
-    public void stop() {
-
-    }
-
-    @Override
-    public void refuel() {
-
-    }
-
-    // Can not be implemented
-    @Override
-    public void openDoors() {
-
+class Motorcycle implements VehicleInterface { 
+    @Override 
+    public void drive() { 
+ 
+    } 
+ 
+    @Override 
+    public void stop() { 
+ 
+    } 
+ 
+    @Override 
+    public void refuel() { 
+ 
     }
 }
