71e711f9adb0628fb4072d30db782778cc9e2e78
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2023-11-08 11:01:31 +0700

    Setelah code Customer diubah

--- cleancode/Customer.java
+++ cleancode/Customer.java
@@ -1,10 +1,10 @@
 package cleancode;
 
-class DtaRcrd102 { 
-    private Date genymdhms; 
-    private Date modymdhms; 
-    private final String pszqint = "102"; 
+class Customer { 
+    private Date generationTimestamp; 
+    private Date modificationTimestamp; 
+    private final String recordId = "102"; 
     /* ... */ 
-} 
+}
        
 
