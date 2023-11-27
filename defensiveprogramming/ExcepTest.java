7d7d0e37711b7d9329738bd88538b7a694ee79a7
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2023-11-27 13:53:58 +0700

    code ExcepTest setelah diubah lagi

--- defensiveprogramming/ExcepTest.java
+++ defensiveprogramming/ExcepTest.java
@@ -1,28 +1,16 @@
 package defensiveprogramming;
 
-import java.io.*;
-
-public class ExcepTest {
-    public static void main(String args[]) {
-        try {
-            int a[] = new int[2];
-            System.out.println("Access element three :" + a[3]);
-
-            // Additional code snippet
-            String fileName = "example.txt";
-            FileInputStream file = null;
-            byte x;
-            try {
-                file = new FileInputStream(fileName);
-                x = (byte) file.read();
-            } catch (IOException i) {
-                i.printStackTrace();
-                System.out.println("Error reading or opening the file.");
-                return;
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Exception thrown :" + e);
-        }
-        System.out.println("Out of the block");
-    }
-}
+public class ExcepTest { 
+    public static void main(String args[]) { 
+    int a[] = new int[2]; 
+    try { 
+    System.out.println("Access element three :" + a[3]); 
+    } catch (ArrayIndexOutOfBoundsException e) { 
+    System.out.println("Exception thrown :" + e); 
+    }finally { 
+    a[0] = 6; 
+    System.out.println("First element value: " + a[0]); 
+    System.out.println("The finally statement is executed"); 
+    } 
+  } 
+} 
